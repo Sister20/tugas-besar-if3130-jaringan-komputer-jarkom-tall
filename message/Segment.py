8f401309f20a43c4097580e90dd25153d031e575
razzanYoni <13521087@mahasiswa.itb.ac.id>
2023-11-27 10:19:16 +0700

    feat : implement checksum

--- message/Segment.py
+++ message/Segment.py
@@ -1,17 +1,17 @@
 import struct
 from message.SegmentFlag import SegmentFlag
 
+
 class Segment():
-    # ini gimana caranya bikin struct di python anjir
-    def __init__(self, flags:int, seq_num:int, ack_num:int, payload:bytes):
+    def __init__(self, flags: int, seq_num: int, ack_num: int, payload: bytes):
         self.seq_num = seq_num
         self.ack_num = ack_num
         self.flags = flags
         self.reserved = 0
         self.payload = payload
+        self.checksum = 0
         self.checksum = self.__calculate_checksum()
 
-    
     def pack(self):
         return struct.pack(">IIBBH32756s",
                            self.seq_num,
@@ -21,38 +21,37 @@ class Segment():
                            self.checksum,
                            self.payload
                            )
-    
+
     @classmethod
     def unpack(cls, packed_data):
         unpacked_data = struct.unpack('>IIBBH32756s', packed_data)
         seq_num, ack_num, flags, reserved, checksum, payload = unpacked_data
         return cls(flags, seq_num, ack_num, payload), checksum
-    
+
     @classmethod
     def unpack_str_payload(cls, packed_data):
         unpacked_data = struct.unpack('>IIBBH32756s', packed_data)
         seq_num, ack_num, flags, reserved, checksum, payload = unpacked_data
         return cls(flags, seq_num, ack_num, payload.decode().strip('\x00')), checksum
 
+    @staticmethod
+    def syn(seq_num: int):
+        return Segment(SegmentFlag.FLAG_SYN, seq_num, 0, "".encode())
 
     @staticmethod
-    def syn(seq_num:int):
-        return Segment(SegmentFlag.FLAG_SYN, seq_num, 0, "".encode())
-    
-    @staticmethod
-    def ack(seq_num:int, ack_num:int):
+    def ack(seq_num: int, ack_num: int):
         return Segment(SegmentFlag.FLAG_ACK, seq_num, ack_num, "".encode())
 
     @staticmethod
-    def syn_ack(seq_num:int, ack_num:int):
+    def syn_ack(seq_num: int, ack_num: int):
         return Segment(SegmentFlag.FLAG_SYN | SegmentFlag.FLAG_ACK, seq_num, ack_num, "".encode())
 
     @staticmethod
-    def fin(seq_num:int):
+    def fin(seq_num: int):
         return Segment(SegmentFlag.FLAG_FIN, seq_num, 0, None)
-    
+
     @staticmethod
-    def fin_ack(seq_num:int, ack_num:int):
+    def fin_ack(seq_num: int, ack_num: int):
         return Segment(SegmentFlag.FLAG_FIN | SegmentFlag.FLAG_ACK, seq_num, ack_num, "".encode())
 
     @staticmethod
@@ -60,14 +59,24 @@ class Segment():
         return Segment(SegmentFlag.FLAG_RST, 0, 0, "".encode())
 
     def __calculate_checksum(self):
-        # TODO: implement
-        return 0
-    
+        # mengubah data segment menjadi data biner
+        data = self.pack()
+        # mengubah nilai checksum menjadi 0
+        data = data[:10] + b'\x00\x00' + data[12:]
+        # mengubah data menjadi daftar bilangan bulat 16 bit
+        words = struct.unpack("!%dH" % (len(data) // 2), data)
+        # menjumlahkan semua kata dengan memperhatikan carry
+        sum = 0
+        for word in words:
+            sum += word
+            if sum > 0xffff:
+                sum = (sum & 0xffff) + 1
+
+        # mengambil satu komplement dari hasil penjumlahan
+        return ~sum & 0xffff
+
     def update_checksum(self):
-        # TODO: implement
-        return 0
-    
+        self.checksum = self.__calculate_checksum()
+
     def is_valid_checksum(self):
-        # TODO: implement
-        return True
-    
+        return self.__calculate_checksum() == self.checksum
